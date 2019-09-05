65b0c7e6613e879ce57a6f77a4f127e0654f4732
Himani Saxena <himani.saxena13@gmail.com>
2019-09-05 14:20:37 -0500

    add

---
+++ HW1_8815/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1_8815
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number 1");
+            string input = Console.ReadLine();//returns string
+            int num1 = int.Parse(input);
+            Console.WriteLine("Enter the number 2");
+            input = Console.ReadLine();//reads line from keybooard and assigns to input variable
+            int num2 = Convert.ToInt32(input);
+            Console.WriteLine("Enter the operator:(+,-,/,*)");
+            string op = Console.ReadLine();
+            if (op == "+")
+            {
+                int tot = num1 + num2;
+                Console.WriteLine("Total is " + tot + ".");
+                Console.ReadLine();
+
+            }
+            if (op == "-")
+            {
+                int sub = num1 - num2;
+                Console.WriteLine("Sub is" + sub + ".");
+                Console.ReadLine();
+            }
+            if (op == "*")
+            {
+                int mul = num1 * num2;
+                Console.WriteLine("Multiplication is" + mul + ".");
+                Console.ReadLine();
+            }
+            if (op == "/")
+            {
+                double div = (num1 / num2);
+                Console.WriteLine("Division is" + div + ".");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
+
